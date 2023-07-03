6336d079ba4f07e54b34983df9e2d58efecf8f6b
kevinbrinsly <brinslykevin@gmail.com>
2023-07-03 10:19:23 +0530

    files

---
+++ login-system/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+//home route
+app.get('/' , (req,res) => {
+  res.render('base', {title:"LoginSystem"});
+
+})
+
+app.listen(port , ()=> {console.log("Listening to the server on http://localhost:3000")});
