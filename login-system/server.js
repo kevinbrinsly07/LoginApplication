cb8c129e28410851188f61532dc7d2738ea81ac0
kevinbrinsly <brinslykevin@gmail.com>
2023-07-03 15:03:16 +0530

    design part

--- login-system/server.js
+++ login-system/server.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
+// load static asset
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 //home route
-app.get('/' , (req,res) => {
-  res.render('base', {title:"Login System"});
+app.get("/", (req, res) => {
+  res.render("base", { title: "Login System" });
+});
 
-})
-
-app.listen(port , ()=> {console.log("Listening to the server on http://localhost:3000")});
+app.listen(port, () => {
+  console.log("Listening to the server on http://localhost:3000");
+});
